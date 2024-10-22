ab80d491be5d42384d3569f1d721a9217e2c6e91
Muatasim <muatasimymiller@gmail.com>
2024-10-22 17:07:25 -0400

    Created branch for flashcard homework

---
+++ script.js
@@ -0,0 +1,19 @@
+const flashcards = [
+    { term: "HTML", definition: "HyperText Markup Language" },
+    { term: "CSS", definition: "Cascading Style Sheets" },
+    { term: "JavaScript", definition: "Programming language of the web" }
+];
+
+
+
+
+let currentIndex = 0;
+let showingTerm = true;
+
+
+function displayCard() {
+
+}
+
+
+window.onload = displayCard;
